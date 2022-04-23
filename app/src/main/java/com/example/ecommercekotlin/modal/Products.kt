e0b46c6630a9fde699f1c7017171c5365b3f9535
washington_candido <washingtoncqc@gmail.com>
2022-04-23 10:30:11 -0300

    adding room dependency

--- app/src/main/java/com/example/ecommercekotlin/modal/Products.kt
+++ app/src/main/java/com/example/ecommercekotlin/modal/Products.kt
@@ -1,9 +1,10 @@
 package com.example.ecommercekotlin.modal
 
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
-
+@Entity
 @Parcelize
 data class Products(
     val name: String,
